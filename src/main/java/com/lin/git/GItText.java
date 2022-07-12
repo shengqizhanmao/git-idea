979c22aba58da96de23f167b85978ebac38c4d60
Lin <1149566912@qq.com>
2022-07-12 18:43:11 +0800

    second commit

--- src/main/java/com/lin/git/GItText.java
+++ src/main/java/com/lin/git/GItText.java
@@ -6,5 +6,6 @@ package com.lin.git;
 public class GItText {
     public static void main(String[] args) {
         System.out.println("hello git1!");
+        System.out.println("hello git2!");
     }
 }
