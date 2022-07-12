adb5e8e9199cdcb4ddfe18b63b12dc1f4acf9e75
Lin <1149566912@qq.com>
2022-07-12 20:15:15 +0800

    Revert "fourth commit main"
                                This reverts commit 12ad58b2

--- src/main/java/com/lin/git/GItText.java
+++ src/main/java/com/lin/git/GItText.java
@@ -8,5 +8,6 @@ public class GItText {
         System.out.println("hello git1!");
         System.out.println("hello git2!");
         System.out.println("hello git3!");
+        System.out.println("hello main!");
     }
 }
